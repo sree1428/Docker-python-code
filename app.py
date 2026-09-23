7ba31abaa00c8f5a87fbf3e9e76dccef6695cf69
sree <sreenooka14@gmail.com>
2026-09-22 20:13:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -63,6 +63,6 @@ def main():
         else:
             print("❌ Invalid choice. Try again.")
 
-
 if __name__ == "__main__":
-    main()
+    port = int(os.getenv("PORT", "8000"))
+    app.run(host="0.0.0.0", port=port, debug=False)
